45460254bd7fe48f2e3cb2852875e56d599a6468
jissac <jaeeysuk@gmail.com>
2022-02-02 09:50:04 -0600

    todo project

---
+++ todo/todo.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"strings"
+	"time"
+	"context"
+	"os"
+	"os/signal"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/thedevsaddam/renderer"
+	mgo "gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
